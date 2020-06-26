d07d54d2e835b44008d6217b335bde996b7ca57a
Linaaaw <lina.henze00@gmail.com>
2020-06-26 09:50:53 +0200

    Kommentare hinzugefügt Funktion verschoben in Continue.gd

--- Scripts/UI/MainMenu/MainMenu.gd
+++ Scripts/UI/MainMenu/MainMenu.gd
@@ -1,24 +1,12 @@
 extends Control
 
-
-func _ready():
-	$MarginContainer/CenterContainer/VBoxContainer/MenuOptions/continue.grab_focus()
-#	$Background. = OS.get_real_window_size()
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	self.rect_size = OS.get_real_window_size()
-	if($MarginContainer/CenterContainer/VBoxContainer/MenuOptions/continue.is_hovered() == true):
-		$MarginContainer/CenterContainer/VBoxContainer/MenuOptions/continue.grab_focus()
-	if($MarginContainer/CenterContainer/VBoxContainer/MenuOptions/options.is_hovered() == true):
-		$MarginContainer/CenterContainer/VBoxContainer/MenuOptions/options.grab_focus()
-	if($MarginContainer/CenterContainer/VBoxContainer/MenuOptions/end.is_hovered() == true):
-		$MarginContainer/CenterContainer/VBoxContainer/MenuOptions/end.grab_focus()
 
-
+# Startet das Spiel
 func _on_continue_pressed():
 	get_tree().change_scene("res://Scenes/Essence.tscn")
 
-
+# Beendet das Spiel
 func _on_end_pressed():
 	get_tree().quit()
