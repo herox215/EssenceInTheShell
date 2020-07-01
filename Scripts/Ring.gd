38eac15e6c5519e921546ca9417f464ff0baa8a3
Lucas Henze <lu.henze@neusta.de>
2020-07-01 10:02:17 +0200

    Ring Refactoring

--- Scripts/Ring.gd
+++ Scripts/Ring.gd
@@ -4,9 +4,11 @@ var _interactionDurationPressed = 0
 var _interractionPressed = false
 
 var _selectedInspectionInteraction = null
+var _selectedInteractInteraction = null
+
 var Player = null
 
-# Updates the ringposition.
+# Aktualisiert die Position des Rings bzw führt Interaktionen aus.
 func _process(delta):
 	var mousePosition = get_local_mouse_position()
 	rotation += mousePosition.angle()
@@ -17,38 +19,50 @@ func _process(delta):
 	if(rotation_degrees < 0):
 		rotation_degrees = 360
 	
-	_interact(delta)
+	# Experimental!!
+	# Wenn die SelectTaste losgelassen wird bevor die Inspection durchgelaufen ist dann wird die Aktion ausgeführt.
+	if(Input.is_action_just_released("ui_interaction") && _interactionDurationPressed < 1):
+		print("Aktion ausführen!")
+		_interactionDurationPressed = 0
 	
-# Returns the angle of the current ringposition.
-func GetAngle():
-	return rotation_degrees
+	_interact(delta)
 
+# Bereinigt die Auswahl.
 func Clear():
 	$SelectionCast.Clear()
 	_selectedInspectionInteraction = null
 
 func _interact(delta):
 	if(Input.is_action_pressed("ui_interaction")):
-		_interractionPressed = true
 		if(_selectedInspectionInteraction != null):
-			$InteractionRing.show()
-			$RingSprite.hide()
+			_interactionMode(true)
 		else:
-			_interactionDurationPressed = 0
-			$InteractionRing.hide()
-			$RingSprite.show()
+			_interactionMode(false)
 	else:
-		_interractionPressed = false
-		_interactionDurationPressed = 0
-		$InteractionRing.hide()
-		$RingSprite.show()
+		
+		_interactionMode(false)
 	
 	if(_interractionPressed && _selectedInspectionInteraction != null):
 		_interactionDurationPressed += delta
 		
 	if(_interactionDurationPressed >= 1 && _selectedInspectionInteraction != null):
+		# An dieser Stelle haben wir nun das Objekt untersucht und können den Command ausführen.
+		# TODO: Aktuell wird hier nur die Message ausgegeben. Gedacht ist es, dass an dieser Stelle ein Command ausgeführt wird.
+		# 		Der "Talk" Command, welcher lediglich eine Textbox mit Text anzeigt. Der Command sollte nur übergeben werden,
+		#		gebaut wird er in der "InspectInteraction".
 		print(_selectedInspectionInteraction.InspectMessage)
 		_interactionDurationPressed = 0
+
+func _interactionMode(enabled):
+	if(enabled):
+		_interractionPressed = true
+		$InteractionRing.show()
+		$RingSprite.hide()
+	else:
+		_interractionPressed = false
+		_interactionDurationPressed = 0
+		$InteractionRing.hide()
+		$RingSprite.show()
 
 func _on_SelectionCast_OnInspectionDeselected():
 	_selectedInspectionInteraction = null
