cbe945d5219a370481af781a4198e7acf8387fad
Lucas Henze <henze94@outlook.com>
2020-07-01 09:07:29 +0200

    InteractionRing hinzugefügt

--- Scripts/Ring.gd
+++ Scripts/Ring.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
+var _interactionDurationPressed = 0
+var _interractionPressed = false
+
 var _selectedInspectionInteraction = null
 var Player = null
 
 # Updates the ringposition.
-func _process(_delta):
+func _process(delta):
 	var mousePosition = get_local_mouse_position()
 	rotation += mousePosition.angle()
 	
@@ -13,7 +16,9 @@ func _process(_delta):
 		rotation_degrees = 0
 	if(rotation_degrees < 0):
 		rotation_degrees = 360
-
+	
+	_interact(delta)
+	
 # Returns the angle of the current ringposition.
 func GetAngle():
 	return rotation_degrees
@@ -22,14 +27,31 @@ func Clear():
 	$SelectionCast.Clear()
 	_selectedInspectionInteraction = null
 
-func _input(event):
-	if(event.get_action_strength("ui_interaction")):
+func _interact(delta):
+	if(Input.is_action_pressed("ui_interaction")):
+		_interractionPressed = true
 		if(_selectedInspectionInteraction != null):
-			print(_selectedInspectionInteraction.InspectMessage)
+			$InteractionRing.show()
+			$RingSprite.hide()
+		else:
+			_interactionDurationPressed = 0
+			$InteractionRing.hide()
+			$RingSprite.show()
+	else:
+		_interractionPressed = false
+		_interactionDurationPressed = 0
+		$InteractionRing.hide()
+		$RingSprite.show()
+	
+	if(_interractionPressed && _selectedInspectionInteraction != null):
+		_interactionDurationPressed += delta
+		
+	if(_interactionDurationPressed >= 1 && _selectedInspectionInteraction != null):
+		print(_selectedInspectionInteraction.InspectMessage)
+		_interactionDurationPressed = 0
 
 func _on_SelectionCast_OnInspectionDeselected():
 	_selectedInspectionInteraction = null
-
 
 func _on_SelectionCast_OnInspectionSelected(inspectionInteraction):
 	_selectedInspectionInteraction = inspectionInteraction
