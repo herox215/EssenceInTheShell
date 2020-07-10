d6799618391ed0f1549eb01af41cffc2477cc0f1
Lucas Henze <lu.henze@neusta.de>
2020-07-10 12:36:17 +0200

    Grass Randomizer hinzugefügt

--- Scripts/Objects/Foliage/Magic Forest/MagicGrass.gd
+++ Scripts/Objects/Foliage/Magic Forest/MagicGrass.gd
@@ -1,16 +1,30 @@
 extends Node2D
 
 var _playerValidator = null
+var _scale = Vector2.ZERO
 
 func _ready():
 	_playerValidator = load("res://Scripts/Misc/ObjectValidator.gd").new()
+	_scale = GetRandomVector()
+	$GrassSprite.set_scale(_scale)
+	
+	var generator = RandomNumberGenerator.new()
+	generator.randomize()
+	if(generator.randf_range(0,2) > 1):
+		$GrassSprite.flip_h = true
 
 
 func _on_Area2D_body_entered(body):
 	if(_playerValidator.IsPlayer(body)):
-		$GrassSprite.set_scale(Vector2(1,0.3))
+		$GrassSprite.set_scale(Vector2(_scale.x, _scale.y - 0.3))
 
 
 func _on_Area2D_body_exited(body):
 	if(_playerValidator.IsPlayer(body)):
-		$GrassSprite.set_scale(Vector2(1,1))
+		$GrassSprite.set_scale(Vector2(_scale.x, _scale.y))
+
+func GetRandomVector():
+	var generator = RandomNumberGenerator.new()
+	generator.randomize()
+	
+	return Vector2(generator.randf_range(0.7, 1.0), generator.randf_range(0.4, 1.2))
